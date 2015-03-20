a3526c3c33c9036e4f92828923d089902ec48a09
Tim James <timbjames@gmail.com>
2015-03-20 17:03:02 +0000

    updated boostrap and changed options

--- anthR.Web/App_Start/BundleConfig.cs
+++ anthR.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace anthR.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap/bootstrap.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
